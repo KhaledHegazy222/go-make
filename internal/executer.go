ea8bccc19178851cd5369ff39d04f68fea13e145
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-10-07 18:12:42 +0300

    Add Project Directory Structure

---
+++ internal/executer.go
@@ -0,0 +1,10 @@
+package internal
+
+func CheckCycle(Target Target) bool {
+
+	return true
+}
+
+func Execute(Target Target) {
+
+}
---
+++ internal/parser.go
@@ -0,0 +1,12 @@
+package internal
+
+type Target struct {
+	Name         string
+	Dependencies []string
+	Commands     []string
+}
+
+func ParseContent(Data []byte) (Targets_list []Target) {
+
+	return nil
+}
