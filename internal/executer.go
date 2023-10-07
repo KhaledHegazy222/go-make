ed2a9e0b974f174ff89c51667fc73d5bb2f9f904
Khaled Hegazy <khaledhegazy870@gmail.com>
2023-10-07 19:45:48 +0300

    Add Executer, Parser Modules

--- internal/executer.go
+++ internal/executer.go
@@ -1,10 +1,59 @@
 package internal
 
-func CheckCycle(Target Target) bool {
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
 
-	return true
+func ContainsCycles(TargetsList []Target) bool {
+	targetsSet := map[string]Target{}
+	for _, target := range TargetsList {
+		targetsSet[target.Name] = target
+	}
+	for _, target := range TargetsList {
+		selectedTargets := map[string]bool{}
+		if isCyclicTarget(target, selectedTargets, targetsSet) {
+			return true
+		}
+	}
+	return false
+}
+
+func isCyclicTarget(selectedTarget Target, scannedTargets map[string]bool, targetsSet map[string]Target) bool {
+	targetExist := scannedTargets[selectedTarget.Name]
+	if targetExist {
+		return true
+	}
+	scannedTargets[selectedTarget.Name] = true
+	if len(selectedTarget.Dependencies) == 0 {
+		return false
+	}
+	for _, dep := range selectedTarget.Dependencies {
+		if targetExist := scannedTargets[dep]; targetExist {
+			return true
+		}
+		if isCyclicTarget(targetsSet[dep], scannedTargets, targetsSet) {
+			return true
+		}
+	}
+	return false
 }
 
 func Execute(Target Target) {
+	for _, command := range Target.Commands {
+		commandSegments := strings.Split(command, " ")
+		prog := commandSegments[0]
+		args := ""
+		if len(commandSegments) > 1 {
+			args = strings.Join(commandSegments[1:], " ")
+		}
+		cmd := exec.Command(prog, args)
+		out, err := cmd.Output()
 
+		if err != nil {
+			fmt.Println("could not run command: ", err)
+		}
+		fmt.Println(string(out))
+	}
 }
--- internal/parser.go
+++ internal/parser.go
@@ -1,12 +1,60 @@
 package internal
 
+import (
+	"strings"
+)
+
 type Target struct {
 	Name         string
 	Dependencies []string
 	Commands     []string
 }
 
-func ParseContent(Data []byte) (Targets_list []Target) {
+func splitTargets(file_lines []string) [][]string {
+	separatedTargets := make([][]string, 0)
+	for _, line := range file_lines {
+		if line == "" {
+			continue
+		}
+		if line[0] != '\t' {
+			// Append New Target
+			newTargetLines := make([]string, 1)
+			newTargetLines[0] = line
+			separatedTargets = append(separatedTargets, newTargetLines)
+		} else {
+			// Append Commands to the last Target
+			separatedTargets[len(separatedTargets)-1] = append(separatedTargets[len(separatedTargets)-1], line)
+		}
 
-	return nil
+	}
+	return separatedTargets
+}
+
+func ParseContent(Data []byte) (TargetsList []Target) {
+	TargetsList = make([]Target, 0)
+	charData := string(Data)
+	lines := strings.Split(charData, "\n")
+	separatedTargets := splitTargets(lines)
+	for _, target := range separatedTargets {
+		addedTarget := Target{}
+
+		line := target[0]
+		lineSegments := strings.Split(line, ":")
+		addedTarget.Name = lineSegments[0]
+		dependenciesNames := strings.Trim(lineSegments[1], " ")
+
+		addedTarget.Dependencies = make([]string, 0)
+		if len(dependenciesNames) >= 1 {
+			for _, dep := range strings.Split(dependenciesNames, " ") {
+				addedTarget.Dependencies = append(addedTarget.Dependencies, strings.Trim(dep, " \t"))
+			}
+		}
+		for _, cmd := range target[1:] {
+			addedTarget.Commands = append(addedTarget.Commands, strings.Trim(cmd, " \t"))
+		}
+
+		TargetsList = append(TargetsList, addedTarget)
+	}
+
+	return TargetsList
 }
